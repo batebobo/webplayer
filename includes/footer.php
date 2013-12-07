085df2f05b7d0355dd5529b8797557d848818d57
bateshot <bateshot@gmail.com>
2013-12-07 19:17:54 +0200

    ..

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <div id="footer">
-            webplayer.com uncopy righted material
+            webplayer.com uncopy righted material </br>
+            This is so boring
         </div>
     
     </body>
